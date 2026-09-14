6ebfb9beda23842ea8cf5b60dcd5df5428948334
gauri-katiyar-codes <gauri.38997@stu.upes.ac.in>
2026-09-14 22:58:28 +0530

    Add files via upload

---
+++ Q26.c
@@ -0,0 +1,32 @@
+//Q26: Write a program to print numbers from 1 to n.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+1 2 3 4 5
+
+Input 2:
+3
+Output 2:
+1 2 3
+
+*/#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (i > 1) {
+            printf(" ");
+        }
+        printf("%d", i);
+    }
+
+    return 0;
+}
