e994d108c916c26724f56efc8a59bfd1b6feafb6
gauri-katiyar-codes <gauri.38997@stu.upes.ac.in>
2026-09-15 19:08:06 +0530

    Add files via upload

---
+++ Q34.c
@@ -0,0 +1,38 @@
+//Q34: Write a program to check if a number is prime.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Prime
+
+Input 2:
+10
+Output 2:
+Not prime
+
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    int is_prime = 1;
+
+    scanf("%d", &number);
+
+    if (number < 2) {
+        is_prime = 0;
+    } else {
+        for (int divisor = 2; divisor <= number / divisor; divisor++) {
+            if (number % divisor == 0) {
+                is_prime = 0;
+                break;
+            }
+        }
+    }
+
+    printf("%s\n", is_prime ? "Prime" : "Not prime");
+    return 0;
+}
